16dc6e7ac2a1b95b63100b0fb06d1400ff028263
Amir Mingaliev <amirmingaliev@gmail.com>
2020-04-17 13:06:06 +0300

    Move validate ship button label to locale

--- app/overrides/spree/admin/users/validation_button.rb
+++ app/overrides/spree/admin/users/validation_button.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   virtual_path: 'spree/admin/users/_addresses_form',
   name: 'add validator button',
   insert_bottom: '[data-hook="ship_address_wrapper"]',
-  text: "<a href='#'' class='button address_validator' style='float: right;'>Validate Ship Address</a>"
+  text: "<a href='#'' class='button address_validator' style='float: right;'><%= t('spree.validate_ship_button') %></a>"
 )
